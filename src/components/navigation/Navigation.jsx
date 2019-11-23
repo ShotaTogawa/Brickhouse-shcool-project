5cefd4e0400b79dbddb26056901fddec23f38b71
Francois Polo <andasan@gmail.com>
2019-11-23 02:56:17 -0800

    -relocated image assets to source (correct me if i'm wrong, I just read it is much better when it comes to building the project) -keeping the Router-DOM in case we might need the Error404 route -cleaning up Nav CSS

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import logo from "../../img/brickhouse.png";
 
 import "./Navigation.scss";
 
@@ -7,50 +8,55 @@ const Navigation = () => {
 
     const NavigationLink = ({ btnObj }) => {
         return btnObj.map((item) => (
-          <div key={item.id}>
-            <NavLink to={item.url} className="btn" activeClassName={item.active}>
-              {item.title}
-            </NavLink>
-          </div>
+            <div className="navigation_item" key={item.id}>
+                <a href={item.url} className="btn">
+                    {item.title}
+                </a>
+            </div>
         ));
-      };
+    };
 
     const navButtons = [
         {
             id: 1,
             title: "ABOUT",
-            active: "active",
+            isActive: false,
             url: "#about_container"
         },
         {
             id: 2,
             title: "TEAM",
-            active: "active",
+            isActive: false,
             url: "#team_container"
         },
         {
             id: 3,
             title: "Press",
-            active: "active",
+            isActive: false,
             url: "#press_container"
         },
         {
             id: 4,
             title: "Contact",
-            active: "active",
+            isActive: false,
             url: "#contact_container"
         }
     ];
 
     return (
         <section className="navigation_container">
-            <div className="navigation_items_left">
-                <NavigationLink btnObj={ [navButtons[0],navButtons[1]] } />
+            <div className="navigation_wrapper">
+            <div className="navigation_items">
+                <NavigationLink btnObj={[navButtons[0], navButtons[1]]} />
             </div>
-            <div className="navigation_logo_main"></div>
-            <div className="navigation_items_right">
-                <a href="#press_container">PRESS</a>
-                <a href="#contact_container">CONTACT</a>
+            <div className="navigation_logo_main">
+                <a href="/">
+                    <img className="brand_logo" src={logo} alt="Brickhouse_Logo" />
+                </a>
+            </div>
+            <div className="navigation_items">
+                <NavigationLink btnObj={[navButtons[2], navButtons[3]]} />
+            </div>
             </div>
         </section>
     );
