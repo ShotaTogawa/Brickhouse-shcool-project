8f6bad26872e0808ec986129e6cf7ddd2cbda5d2
Francois Polo <andasan@gmail.com>
2019-11-22 21:38:24 -0800

    change tag to section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Hero.scss";
 
 const Hero = () => {
-  return <div className="hero_container">
+  return <section className="hero_container">
       <div className="hero_title">SEEKING OPPORTUNITIES <br/><span>IN EARLY STAGE ESPORTS, SPORTS & ENTERTAINMENT TECHNOLOGIES</span></div>
-  </div>
+  </section>
 };
 
 export default Hero;
