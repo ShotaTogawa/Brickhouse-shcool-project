8988a52dce736a8c1330f3ff5cb30760d7234b3e
Francois Polo <andasan@gmail.com>
2019-11-22 21:13:24 -0800

    - Import Roboto font to global CSS (includes regular, medium, bold and black) - added placeholder for Hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./Hero.scss";
 
 const Hero = () => {
-  return <div className="hero_container"></div>;
+  return <div className="hero_container">
+      <div className="hero_title">SEEKING OPPORTUNITIES <br/><span>IN EARLY STAGE ESPORTS, SPORTS & ENTERTAINMENT TECHNOLOGIES</span></div>
+  </div>
 };
 
 export default Hero;
