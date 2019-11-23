60e6d692f792705e51d4aa893b5baf4f9b01b939
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-22 21:05:07 -0800

    finished basic layout

--- src/components/press/Press.jsx
+++ src/components/press/Press.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "./Press.scss";
 
+const countBodyNum = body => {
+  console.log(body.length);
+  if (body.length >= 140) {
+    return body.slice(0, 140) + "...";
+  }
+  return body;
+};
+
 const Press = ({ press }) => {
-  console.log(press);
   return (
     <div className="card">
-      <img src={press.image} class="card-image" />
+      <img src={press.image} className="card-image" />
       <ul className="card-list">
         <li>{press.category}</li>
         <li>{press.date}</li>
       </ul>
       <h2 className="card-title">{press.title}</h2>
-      <p className="card-body">{press.body}</p>
+      <p className="card-body">{countBodyNum(press.body)}</p>
+      <div className="card-author">
+        <img src={press.avatar} className="card-avatar" alt={press.author} />
+        <ul className="card-author-info">
+          <li>{press.company}</li>
+          <li>{press.author}</li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/press/PressList.jsx
+++ src/components/press/PressList.jsx
@@ -14,9 +14,10 @@ class PressList extends Component {
           "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLgb68i4i-SdxNtSX6kBDpKVW3XEM2JQiRMKyu-ggRW8qByj-g",
-        avatar: "",
-        author: "",
-        company: ""
+        avatar:
+          "https://s3-media2.fl.yelpcdn.com/bphoto/ZmA5A0e_74G1qUZSiqmvHQ/ls.jpg",
+        author: "Steave Kim",
+        company: "CICCC"
       },
       {
         id: 2,
@@ -27,9 +28,10 @@ class PressList extends Component {
           "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLgb68i4i-SdxNtSX6kBDpKVW3XEM2JQiRMKyu-ggRW8qByj-g",
-        avatar: "",
-        author: "",
-        company: ""
+        avatar:
+          "https://s3-media2.fl.yelpcdn.com/bphoto/ZmA5A0e_74G1qUZSiqmvHQ/ls.jpg",
+        author: "Steave Kim",
+        company: "CICCC"
       },
       {
         id: 3,
@@ -40,9 +42,10 @@ class PressList extends Component {
           "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLgb68i4i-SdxNtSX6kBDpKVW3XEM2JQiRMKyu-ggRW8qByj-g",
-        avatar: "",
-        author: "",
-        company: ""
+        avatar:
+          "https://s3-media2.fl.yelpcdn.com/bphoto/ZmA5A0e_74G1qUZSiqmvHQ/ls.jpg",
+        author: "Steave Kim",
+        company: "CICCC"
       },
       {
         id: 4,
@@ -50,12 +53,13 @@ class PressList extends Component {
         date: "week ago",
         title: "Nemo enim ipsam, Tempor incididunt",
         body:
-          "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
+          "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur. ",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLgb68i4i-SdxNtSX6kBDpKVW3XEM2JQiRMKyu-ggRW8qByj-g",
-        avatar: "",
-        author: "",
-        company: ""
+        avatar:
+          "https://s3-media2.fl.yelpcdn.com/bphoto/ZmA5A0e_74G1qUZSiqmvHQ/ls.jpg",
+        author: "Steave Kim",
+        company: "CICCC"
       }
     ]
   };
