865f6a9c7ae5f62c0801d03cb00a7c1b3d750c86
JamesYang <gn00999829@gmail.com>
2019-11-26 14:12:07 -0800

    updated box-sizing for css

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./hero/Hero";
 import PressList from "./press/PressList";
 import About from "./about/About";
+import Partners from "./partners/Partners";
 
 function IndexPage() {
     return (
         <>
             <Hero />
             <About />
+            <Partners />
             <PressList />
         </>
   );
