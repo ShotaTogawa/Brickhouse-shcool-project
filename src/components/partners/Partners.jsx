c22b4b071bf4a94cd673435019c4a8838c5aa3f4
JamesYang <gn00999829@gmail.com>
2019-11-26 14:46:18 -0800

    test

---
+++ src/components/partners/Partners.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Partners.scss";
+import Card from "../partners/card/Card";
+
+
+const Partners = () => {
+    return (
+        <section className="partners_container" id="team">
+            <h1 className="partners_title">PARTNERS</h1>
+            <Card />
+        </section>
+    )
+}
+
+export default Partners;
---
+++ src/components/partners/card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Card.scss";
+
+const Card = () => {
+    return (
+        <div className="partners_cards_container">
+            <div className="partners_cards_card_container">
+                <div className="partners_cards_card_left_container">
+                    <div>
+
+                    </div>
+                </div>
+                <div className="partners_cards_card_right_container">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
