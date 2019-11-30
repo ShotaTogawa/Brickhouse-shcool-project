f4c294ca218227acc387d86dec8dca37c3354cb2
40 daimon <tvwmdaimon40@gmail.com>
2019-11-29 16:22:38 -0800

    recreate slider with react-slick

--- src/components/advisors/advisorsInfo.jsx
+++ src/components/advisors/advisorsInfo.jsx
@@ -1,90 +1,88 @@
-const data = {
-    advisors: [
-        {
-            index: 0,
-            name: "JAMES MURRAY",
-            location: "Chicago",
-            description: "Mr. Murray is a Partner and Head of Business Development at Centric Wealth Management in Chicago. Previously at Merrill Lynch, Jim was one of the founding employees at Centric and has been instrumental in building the athlete division at Centric which now includes over 25 athletes in multiple sports. Jim played 4 years at St. Cloud State, a top Division 1 Men’s Ice Hockey Program.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 1,
-            name: "JAS BAGRY",
-            location: "Vancouver/NYC",
-            description: "Mr. Bagry is a seasoned finance professional and entrepreneur with over 10 years experience in fund management and advisory services. In his current role, his acts as Managing Director for family owned investments and asset management. Previously, he was VP at New Market Funds, where he helped raise $65 million and help with business strategy, capital raises, M&A, and valuations. He is also a CFA Charterholder.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 2,
-            name: "JOSEPH CLEMKO",
-            location: "Dallas",
-            description: "Mr. Clemko, a lawyer with Stinson Law, combines his work experience with his legal and business background to counsel business, start-ups, entrepreneurs and investors, including private equity fund formation and capital raising. Clemko focuses on emerging technologies, blockchain, cryptocurrency, AI, esports, and gaming. Previously, he acted as a Regulatory Compliance Associate for Forex Capital Markets.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 3,
-            name: "MARK NAUSHA",
-            location: "Dallas",
-            description: "Mr. Nausha is a noted game developer, online community builder, entrepreneur and current Deputy Director of the Game Lab at Southern Methodist University Guildhall. He has shipped more than 70 titles commercially in the gamingindustry, generating billions of dollars in revenue. Mark is also President at EGENCY and an adjunct professor at SMU Guildhall.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 4,
-            name: "ERIC JOHNSON",
-            location: "LA",
-            description: "Mr. Johnson is CEO and founder of Won Worldwide, a strategic revenue advisory firm dedicated to navigating the Global Sports and Entertainment Media landscape. He is also an active Board Member and Faculty Director at UCLA Anderson. Previously, he was Executive Vice President of Global Advertising Revenueand Sales Operations at ESPN",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 5,
-            name: "BRODIE GUNNING",
-            location: "Vancouver",
-            description: "Mr. Gunning is an experienced entrepreneur with over 14 years operating ventures in both Asia, North and Central America. His experience covers ventures in digital media, drones, IOT, disaster response, tech incubators, hemp processing, fintech and cryptocurrency. He is currently a Director of Incubara Capital an investment company focussed on disruptive technologies andventure building company focused on investingand growing early stage companies",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 6,
-            name: "DION POUNCIL",
-            location: "LA",
-            description: "Mr. Pouncil is the chairman of Playbook Network, a Live & On Demand video network focused on covering money, tech, real estate, and innovative products & services. Prior to this, he lead high preforming teams for Fortune 500 companies and start-ups, instrategy consulting in tech, media, and entertainment.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 7,
-            name: "VOHARA GUNARATNA",
-            location: "Vancouver",
-            description: "Mr. Gunaratna is Vice President of Sales at Buyatab Online Inc. and an active advisor to early stage technology companies. He was one of the founding members of Buyatab, and over the past 8+ years, has lead to the creation and execution of the sales strategies, business development, strategic partnerships, and contract negotiations.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 8,
-            name: "TARRNIE WILLIAMS",
-            location: "Vancouver",
-            description: "Mr. Williams is a highly experienced executive, entrepreneur, and creative leader who has dedicated his career to building products on thefore front of interactive technology. He has led development on almost every platform launch, new device, and business model, and has helped build and operate multiple successful studios and teams, including Electronic ArtsCanada and LA, Relic Entertainment, and Roadhouse Interactive. Tarrnie is CEO and co-founder of Blueprint Reality Inc., a mixed reality media company connecting VR and AR with social media and streaming.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 9,
-            name: "ISRAEL PONS",
-            location: "Mexico City",
-            description: "Mr. Pons is CEO and co-founding partner of Angels Nest LATAM, an angel investment network operating in Mexico and Latin American countries. Cited by XCALA as themost active angel network in Latin America, having over 400 angels in the network. Israelis also President of the National Entrepreneurship Committee of Mexican Institute of Financial Executives (IMEF)",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 10,
-            name: "MARK MCCARTNEY",
-            location: "Berlin",
-            description: "Mr. McCartney has over 10 years experience, his previous roles include corporate development, management consultancy, investor relations, corporate banking analyst, and he has served as a Director of TSX. Vlisted companies. Having previously worked in various sectors for firms in Dublin, Londonand Vancouver, he has built a business network in areas of finance, banking, mining, biotech and technology.",
-            image: "./img/smiley.png"
-        },
-        {
-            index: 11,
-            name: "ANNA FARRANT",
-            location: "Vancouver",
-            description: "Ms. Farrant co-founded All City Athletics in 2014. The company is a boutique boxing studio located in the Gastown area of Vancouver BC. She also currently sits on the board of Boxing BC, the provincial governingbody, as the Vice President. Prior to All City she served on the board of Eastside Boxing and helped design their curriculum. She recently launched Alliance Boxing Promotions a company that produces premiere boxing matches.",
-            image: "./img/smiley.png"
-        }
-    ]
-};
+const advisors = [
+    {
+        name: "JAMES MURRAY",
+        location: "Chicago",
+        description:
+        "Mr. Murray is a Partner and Head of Business Development at Centric Wealth Management in Chicago. Previously at Merrill Lynch, Jim was one of the founding employees at Centric and has been instrumental in building the athlete division at Centric which now includes over 25 athletes in multiple sports. Jim played 4 years at St. Cloud State, a top Division 1 Men’s Ice Hockey Program.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "JAS BAGRY",
+        location: "Vancouver/NYC",
+        description:
+        "Mr. Bagry is a seasoned finance professional and entrepreneur with over 10 years experience in fund management and advisory services. In his current role, his acts as Managing Director for family owned investments and asset management. Previously, he was VP at New Market Funds, where he helped raise $65 million and help with business strategy, capital raises, M&A, and valuations. He is also a CFA Charterholder.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "JOSEPH CLEMKO",
+        location: "Dallas",
+        description:
+        "Mr. Clemko, a lawyer with Stinson Law, combines his work experience with his legal and business background to counsel business, start-ups, entrepreneurs and investors, including private equity fund formation and capital raising. Clemko focuses on emerging technologies, blockchain, cryptocurrency, AI, esports, and gaming. Previously, he acted as a Regulatory Compliance Associate for Forex Capital Markets.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "MARK NAUSHA",
+        location: "Dallas",
+        description:
+        "Mr. Nausha is a noted game developer, online community builder, entrepreneur and current Deputy Director of the Game Lab at Southern Methodist University Guildhall. He has shipped more than 70 titles commercially in the gamingindustry, generating billions of dollars in revenue. Mark is also President at EGENCY and an adjunct professor at SMU Guildhall.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "ERIC JOHNSON",
+        location: "LA",
+        description:
+        "Mr. Johnson is CEO and founder of Won Worldwide, a strategic revenue advisory firm dedicated to navigating the Global Sports and Entertainment Media landscape. He is also an active Board Member and Faculty Director at UCLA Anderson. Previously, he was Executive Vice President of Global Advertising Revenueand Sales Operations at ESPN",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "BRODIE GUNNING",
+        location: "Vancouver",
+        description:
+        "Mr. Gunning is an experienced entrepreneur with over 14 years operating ventures in both Asia, North and Central America. His experience covers ventures in digital media, drones, IOT, disaster response, tech incubators, hemp processing, fintech and cryptocurrency. He is currently a Director of Incubara Capital an investment company focussed on disruptive technologies andventure building company focused on investingand growing early stage companies",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "DION POUNCIL",
+        location: "LA",
+        description:
+        "Mr. Pouncil is the chairman of Playbook Network, a Live & On Demand video network focused on covering money, tech, real estate, and innovative products & services. Prior to this, he lead high preforming teams for Fortune 500 companies and start-ups, instrategy consulting in tech, media, and entertainment.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "VOHARA GUNARATNA",
+        location: "Vancouver",
+        description:
+        "Mr. Gunaratna is Vice President of Sales at Buyatab Online Inc. and an active advisor to early stage technology companies. He was one of the founding members of Buyatab, and over the past 8+ years, has lead to the creation and execution of the sales strategies, business development, strategic partnerships, and contract negotiations.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "TARRNIE WILLIAMS",
+        location: "Vancouver",
+        description:
+        "Mr. Williams is a highly experienced executive, entrepreneur, and creative leader who has dedicated his career to building products on thefore front of interactive technology. He has led development on almost every platform launch, new device, and business model, and has helped build and operate multiple successful studios and teams, including Electronic ArtsCanada and LA, Relic Entertainment, and Roadhouse Interactive. Tarrnie is CEO and co-founder of Blueprint Reality Inc., a mixed reality media company connecting VR and AR with social media and streaming.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "ISRAEL PONS",
+        location: "Mexico City",
+        description:
+        "Mr. Pons is CEO and co-founding partner of Angels Nest LATAM, an angel investment network operating in Mexico and Latin American countries. Cited by XCALA as themost active angel network in Latin America, having over 400 angels in the network. Israelis also President of the National Entrepreneurship Committee of Mexican Institute of Financial Executives (IMEF)",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "MARK MCCARTNEY",
+        location: "Berlin",
+        description:
+        "Mr. McCartney has over 10 years experience, his previous roles include corporate development, management consultancy, investor relations, corporate banking analyst, and he has served as a Director of TSX. Vlisted companies. Having previously worked in various sectors for firms in Dublin, Londonand Vancouver, he has built a business network in areas of finance, banking, mining, biotech and technology.",
+        image: "./img/smiley.png"
+    },
+    {
+        name: "ANNA FARRANT",
+        location: "Vancouver",
+        description:
+        "Ms. Farrant co-founded All City Athletics in 2014. The company is a boutique boxing studio located in the Gastown area of Vancouver BC. She also currently sits on the board of Boxing BC, the provincial governingbody, as the Vice President. Prior to All City she served on the board of Eastside Boxing and helped design their curriculum. She recently launched Alliance Boxing Promotions a company that produces premiere boxing matches.",
+        image: "./img/smiley.png"
+    }
+];
 
-export default data;
+        export default advisors;  
