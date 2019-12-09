a81b8defd071564b420c202f0c2aaca1590f8fc7
JamesYang <gn00999829@gmail.com>
2019-12-09 13:59:46 -0800

    fixed favicon, manifest, meta data, linkedin link

--- src/components/footer/Icons.jsx
+++ src/components/footer/Icons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Icons = ({ link,img }) => {
   return (
     <div>
-      <a href={link}><img className="footer_icon_items" src={img} alt="social_icons"/></a>
+      <a href={link} target="_blank"><img className="footer_icon_items" src={img} alt="social_icons"/></a>
     </div>
   );
 };
